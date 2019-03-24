ebb4e3a3fb7ef8c8d7cc4adf4a400d4498ba9903
Никита Алексеев <supersonicrock2@gmail.com>
2019-03-24 13:45:47 +0400

    Исправление ошибок после первой защиты

--- js/script.js
+++ js/script.js
@@ -7,33 +7,33 @@ var emailFeedback = modal.querySelector(".modal-email");
 var textFeedback = modal.querySelector(".modal-text"); 
 var buttonFeedback = modal.querySelector(".button-feedback");
 
-      modalOpen.addEventListener("click", function (evt) {
-        evt.preventDefault();
-        modal.classList.add("modal-show");
-        nameUserFeedback.focus();
-      });
+modalOpen.addEventListener("click", function (evt) {
+	evt.preventDefault();
+	modal.classList.add("modal-show");
+	nameUserFeedback.focus();
+});
 
-      modalClose.addEventListener("click", function (evt) {
-        evt.preventDefault();
-        modal.classList.remove("modal-show");
-        modal.classList.remove("modal-error");
-      });
+modalClose.addEventListener("click", function (evt) {
+	evt.preventDefault();
+	modal.classList.remove("modal-show");
+	modal.classList.remove("modal-error");
+});
 
-      window.addEventListener("keydown", function(evt) {
-        if (evt.keyCode === 27) {
-          if (modal.classList.contains("modal-show")) {
-            evt.preventDefault();
-            modal.classList.remove("modal-show");
-            modal.classList.remove("modal-error");
-          }
-        }
-      });
+window.addEventListener("keydown", function(evt) {
+	if (evt.keyCode === 27) {
+		if (modal.classList.contains("modal-show")) {
+			evt.preventDefault();
+			modal.classList.remove("modal-show");
+			modal.classList.remove("modal-error");
+		}
+	}
+});
 
-      formFeedback.addEventListener("submit", function (evt) {
-        if (!nameUserFeedback.value || !emailFeedback.value || !textFeedback.value) {
-          evt.preventDefault();
-          buttonFeedback.classList.remove("modal-error");
-          buttonFeedback.offsetWidth = buttonFeedback.offsetWidth;
-          buttonFeedback.classList.add("modal-error");
-        }
-      });
+formFeedback.addEventListener("submit", function (evt) {
+	if (!nameUserFeedback.value || !emailFeedback.value || !textFeedback.value) {
+		evt.preventDefault();
+		buttonFeedback.classList.remove("modal-error");
+		buttonFeedback.offsetWidth = buttonFeedback.offsetWidth;
+		buttonFeedback.classList.add("modal-error");
+	}
+});
